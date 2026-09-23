dd4e999aec65d8e5fec4bf97a19fb2970f3d69d4
Mansi Attri <80748107+mansiattrii@users.noreply.github.com>
2026-09-23 18:52:38 +0530

    Time: 26 ms (62.23%), Space: 16.1 MB (96.47%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,47 +1,28 @@
 class Solution {
 public:
-    void bfs(int row, int col, vector<vector<int>>& vis, vector<vector<char>>& grid){
-        vis[row][col] = 1;
-        queue<pair<int,int>> q;
-        q.push({row, col});
+//this approach uses dfs and we won't be using any additional space to store visited. we'll be flipping the bits in grid that we traverse
+    void consumeIsland(vector<vector<char>>& grid, int i, int j){
+        if(i<0 || j<0 || i>=grid.size() || j>=grid[0].size() || grid[i][j] != '1')    return;
 
-        while(!q.empty()){
-            row = q.front().first;
-            col = q.front().second;
-            q.pop();
-
-            //going to every neighbour of our current element {row, col}: {row-1, col}, {row+1, col}, {row, col+1} and {row, col-1}.
-            for(int i = -1; i<=1; i++){
-                for(int j = -1; j<=1; j++){
-                    //skipping all the diagonal elements:
-                    if(i==-1 && j==-1 || i==1 && j==1 || i==1 && j==-1 ||i==-1 && j==1) continue;
-                    int row1 = row + i;
-                    int col1 = col + j;
-
-                    //check out of bound conditions, if the element has been visited before and if its land.
-                    if(row1>=0 && row1<grid.size() && col1>=0 && col1<grid[0].size() && !vis[row1][col1] && grid[row1][col1] == '1'){
-                        //mark the neighbour visited and add it to queue for processing further.
-                        vis[row1][col1] = 1;
-                        q.push({row1, col1});
-                    }
-                }
-            }
-        }
+        grid[i][j] = '0';
+        consumeIsland(grid, i-1, j);
+        consumeIsland(grid, i, j-1);
+        consumeIsland(grid, i, j+1);
+        consumeIsland(grid, i+1, j);
     }
     int numIslands(vector<vector<char>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        vector<vector<int>> vis(n, vector<int>(m,0));
-        int cnt = 0;
+        int islands = 0;
 
-        for(int row = 0; row < n; row++){
-            for(int col = 0; col < m; col++){
-                if(!vis[row][col] && grid[row][col] == '1'){
-                    bfs(row, col, vis, grid);
-                    cnt++;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(grid[i][j] == '1'){
+                    islands++;
+                    consumeIsland(grid, i, j);
                 }
             }
         }
-        return cnt;
+        return islands;
     }
 };
