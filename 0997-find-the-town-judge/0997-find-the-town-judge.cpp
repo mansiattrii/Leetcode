cb42e73558fbdeb78de804f3a8703a1b9b62d846
Mansi Attri <80748107+mansiattrii@users.noreply.github.com>
2026-09-24 19:18:06 +0530

    Time: 41 ms (13.39%), Space: 73.2 MB (34.83%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.cpp
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
-        vector<vector<int>> q(n+1);
-        unordered_map<int, int> mpp(n+1);
-
-        //create adjacency list and calculate the frequecy-
+        vector<int> delta(n+1, 0);
         for(auto it : trust){
-            q[it[0]].push_back(it[1]);
-            mpp[it[1]]++;
+            int out = it[0];
+            int in = it[1];
+            delta[out]--;
+            delta[in]++;
         }
-        for(int i = 1; i<=n; i++){
-            if(mpp[i] == n-1 && q[i].size() == 0){
+        for(int i=1; i<n+1; i++){
+            if(delta[i] == n-1)
                 return i;
-            }
         }
         return -1;
     }
